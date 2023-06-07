fd19858e9829b899cfa54f6d4a764b28a51a329b
Trần Vũ Lộc <loctran125.works@gmail.com>
2023-06-07 16:06:52 +0700

    update description

--- server.js
+++ server.js
@@ -1,11 +1,8 @@
-const express = require('express')
+const express = require("express");
 
-
-const app = express()
-
+const app = express();
 
 app.use(express.static(__dirname));
-
 
 const PORT = process.env.PORT || 3000;
 
